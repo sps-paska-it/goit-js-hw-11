16bda63499fb96953effa50c009b5f7e8fa063b2
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-03-05 22:03:36 +0200

    correct after mentor

--- src/js/index.js
+++ src/js/index.js
@@ -2,40 +2,41 @@ import '../css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import { fetchCountries } from '../js/fetchCountries';
-import { renderFunctions } from './render_functions'
-import { refs } from "./refs";
+import { renderFunctions } from './render_functions';
+import { refs } from './refs';
 
 const DEBOUNCE_DELAY = 300;
 
 const onSearchCountry = e => {
   const inputValue = e.target.value.trim();
+  refs.listCountries.innerHTML = '';
+  refs.cardCountry.innerHTML = '';
   if (inputValue !== '') {
     fetchCountries(inputValue).then(filterCorrectInput).catch(onFetchError);
   }
 };
 
 const toMuchMatches = () => {
-  Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
 };
 
 const onFetchError = () => {
-Notiflix.Notify.failure('Oops, there is no country with that name');
+  Notiflix.Notify.failure('Oops, there is no country with that name');
 };
 
 const filterCorrectInput = response => {
-refs.listCountries.innerHTML = '';
-refs.cardCountry.innerHTML = '';
   if (response.length > 10) {
     console.log(response.length);
-   return toMuchMatches();
+    return toMuchMatches();
   }
   renderFunctions(response);
 };
 
 const onMarkupCardCountry = response => {
-console.log(response, 'card');
+  console.log(response, 'card');
 };
-
 
 refs.inputSearch.addEventListener(
   'input',
